1fd14782da5b65076da6ccce4d61674e5fac4541
zhenmai <zhenmaihu2017@gmail.com>
2020-02-20 20:22:57 -0800

    Graph structure to store edges and vertexes

--- Graph.hpp
+++ Graph.hpp
@@ -1,46 +1,35 @@
 
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <list>
-#include <array>
-#include <algorithm>
-#include <iomanip>
-#include <cmath>
 #include <vector>
-#include <functional>
-#include <cstdlib>
-#include <string>
 
 // data structure to store graph edges
 struct Edge 
 {
-	unsigned int src, dest;
-}
-
-// data structure to store pagerank values
-std::vector<float> pr_value;
+	unsigned src, dest;
+};
 
 // class to represent a graph object
 class Graph
 {
 public:
-	// construct a vector of vectors to represent an adjacency list
-	std::vector<std::vector<int>> adjList;
+	// construct a vector of vectors to represent each edge index and its outhoing edges
+	std::vector<std::vector<int>> adjEdges;
 
 	// Graph Constructor
-	Graph(std::vector<Edge> const &edges, int Num)
+	Graph(unsigned Num, std::vector<Edge> &input): vertex_num(Num), edges(input)
 	{
-		adjList.resize(Num);
+		adjEdges.resize(Num);
 		// add edges to the directed graph
 		for (auto &edge: edges)
 		{
-			adjList[edge.src].push_back(edge.dest);
+			adjEdges[edge.src].push_back(edge.dest);
 		}
 	}
-	unsigned count_edges(unsigned i) { return adjList[i].size(); }
-	unsigned vertex_count() {return vertex_num;}
+
+	unsigned OutgoingEdgeCount(unsigned i) { return adjEdges[i].size(); }
+	unsigned VertexesNum() {return vertex_num;}
+
 private:
-	
 	unsigned int vertex_num;	
-}
+	std::vector<Edge> const &edges;
+};
