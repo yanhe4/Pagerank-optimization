0f8ced3328fadc8b48d75d3b394072d1c790340b
zhenmai <zhenmaihu2017@gmail.com>
2020-02-21 16:18:21 -0800

    Update graph to store inward edges and count outgoing edges

--- Graph.hpp
+++ Graph.hpp
@@ -12,24 +12,29 @@ struct Edge
 class Graph
 {
 public:
-	// construct a vector of vectors to represent each edge index and its outhoing edges
+	// construct a vector of vectors to represent each edge index and its inward edges
 	std::vector<std::vector<int>> adjEdges;
 
 	// Graph Constructor
 	Graph(unsigned Num, std::vector<Edge> &input): vertex_num(Num), edges(input)
 	{
 		adjEdges.resize(Num);
+		outgoing_edges_num.resize(Num);
 		// add edges to the directed graph
 		for (auto &edge: edges)
 		{
-			adjEdges[edge.src].push_back(edge.dest);
+			adjEdges[edge.dest].push_back(edge.src);
+			outgoing_edges_num[edge.src]++;
 		}
 	}
 
-	unsigned OutgoingEdgeCount(unsigned i) { return adjEdges[i].size(); }
+	unsigned InwardEdgeCount(unsigned i) { return adjEdges[i].size();}
+	unsigned OutgoingEdgeCount(unsigned i) { return outgoing_edges_num[i]; }
 	unsigned VertexesNum() {return vertex_num;}
 
 private:
 	unsigned int vertex_num;	
 	std::vector<Edge> const &edges;
+	// construct a vector to store outgoing edges for each source index
+	std::vector<int> outgoing_edges_num;
 };
